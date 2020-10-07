55dfd618132fd535e1d7a42fde6a89cac6c023e5
= <=>
2020-10-06 22:25:53 -0500

    Initial commit, start project for Masiv

---
+++ src/main/java/com/masiv/roulette/RouletteBetApplication.java
@@ -0,0 +1,13 @@
+package com.masiv.roulette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouletteBetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouletteBetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/masiv/roulette/RouletteBetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masiv.roulette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouletteBetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
